02fcf900d7bede6da7f067e1b58707e78782f83c
dsabirbaev <davran9559@gmail.com>
2024-03-11 19:00:20 +0500

    done intro

---
+++ assets/images/index.tsx
@@ -0,0 +1,8 @@
+
+
+
+import first from  "@/assets/images/clear.png";
+import second from  "@/assets/images/alpen.webp";
+import third from  "@/assets/images/zateya.jpeg";
+
+export {first, second, third}
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
+"use client";
+
+import Image from 'next/image';
+import { Carousel, CarouselResponsiveOption } from 'primereact/carousel';
+import { first, second, third } from '@/assets/images';
+
+import "./style.css"
 
 const Intro = () => {
+  
+  const products: string[] = [first, second, third];
+
+  const responsiveOptions: CarouselResponsiveOption[] = [
+      {
+          breakpoint: '1400px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '1199px',
+          numVisible: 3,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '575px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
+
+  const productTemplate = (products: string) => {
+    return (
+        <div className="border-1 surface-border  m-2 text-center w-full">
+           <Image src={products} width={1100} height={250} alt="image" className='rounded-xl w-full' />
+        </div>
+    );
+  };
   return (
-    <section>
+    <section id='intro' className='py-3'>
       <div className='container'>
-        intro
+        <div>
+          <Carousel value={products} showIndicators={false} autoplayInterval={3000} numVisible={1} numScroll={1} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
+        </div>
       </div>
     </section>
   )
